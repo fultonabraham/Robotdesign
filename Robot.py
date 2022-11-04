18c8e48447f907f08c6dfe7bc408ca4a1388ebf9
fultonabraham <115903676+fultonabraham@users.noreply.github.com>
2022-11-04 00:15:24 -0400

    Update Robot.py

--- Robot.py
+++ Robot.py
@@ -4,9 +4,9 @@ This is the Robot class
 
 class Robot:
     
-    def __init__(self, MovementControl, MovementObjectControl, Camera, System, Engine) -> None:
+    def __init__(self, MovementControl, ArmControl, Camera, System, Engine) -> None:
         self._MovementControl = MovementControl
-        self._MovementObjectControl = MovementObjectControl
+        self._ArmControl = ArmControl
         self._Camera = Camera
         self._System = System
         self._Engine = Engine
